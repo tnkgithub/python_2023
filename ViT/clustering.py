0dba59381b3a926d875fe437d3342c8737f9bc2e
Shumpei Tanaka <ShumpeiTanaka413@gmail.com>
2024-04-16 12:21:18 +0900

    a

--- ViT/clustering.py
+++ ViT/clustering.py
@@ -8,27 +8,27 @@ import os
 import datetime
 
 # %%
-df = pd.read_csv("./result_feature/feature_vit.csv", index_col=0)
-# df = pd.read_csv("./result_feature_pca/resnet_pca.csv", index_col=0)
-# new_index = []
-# for i in range(len(df)):
-#     new_name = df.index[i].split("-")[0]
-#     new_name = new_name[:8]
-#     new_index.append(new_name)
+# df = pd.read_csv("./result_feature/feature_vit.csv", index_col=0)
+df = pd.read_csv("./result_feature_pca/resnet_pca.csv", index_col=0)
+new_index = []
+for i in range(len(df)):
+    new_name = df.index[i].split("-")[0]
+    new_name = new_name[:8]
+    new_index.append(new_name)
 img_dir_path = "/home/b1019035/2023/python_2023/scraping/images"
 
 # %%
 pdist = pdist(df, metric="cosine")
 ward_cos = linkage(pdist, method="ward", metric="cosine")
 plt.figure(figsize=(20, 10), num=None, dpi=80, facecolor="w", edgecolor="k")
-dendrogram(ward_cos, labels=df.index, leaf_font_size=8)
+dendrogram(ward_cos, labels=new_index, leaf_font_size=8)
 
 # %%
 bucket = [0] * 2000  # クラスタ数、サイズが分からないので40としておく
 cluster_num = 0
 clusters_size = [0]
 
-fclusters = fcluster(ward_cos, 0.5, criterion="distance")
+fclusters = fcluster(ward_cos, 1.0, criterion="distance")
 
 # クラスターの数と各クラスターのサイズを求める
 for i in fclusters:
@@ -46,22 +46,22 @@ print(clusters_size)
 # %%
 cluster_list = [[] for i in range(cluster_num + 1)]
 for i in range(len(fclusters)):
-    img_path = os.path.join(img_dir_path, df.index[i])
+    img_path = os.path.join(img_dir_path, new_index[i])
     img_path = img_path + ".jpg"
     print(img_path)
     cluster_list[fclusters[i]].append(img_path)
 
 #%%
-os.mkdir('./result_cluster_vit_1.0')
+os.mkdir('./result_cluster_resnet_1.5')
 
 # %%
-m = 5
-n = 5
+m = 10
+n = 10
 imgs = [0] * m * n
 now = datetime.datetime.now()
 for clust_no in range(1, cluster_num):
     img_name = (
-        "./result_cluster_vit_1.0/ward_cos_"
+        "./result_cluster_resnet_1.0/ward_cos_"
         + str(clust_no)
         + "_"
         + now.strftime("%Y%m%d_%H%M%S")
